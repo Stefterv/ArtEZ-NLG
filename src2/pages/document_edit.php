85c9caef26b24c25ab4e4c1bc86e5c332595bd49
Willem Kempers <willemkempers@gmail.com>
2015-05-14 16:28:30 +0200

    sorting

--- src2/pages/document_edit.php
+++ src2/pages/document_edit.php
@@ -14,15 +14,35 @@
 			<input type="text" name="title" value="<?=$document->title?>" placeholder="Document Title">
 			<input type="text" name="author" value="<?=$document->author?>"  placeholder="Document Author">
 			<input type="text" name="based_on" value="<?=$document->based_on?>"  placeholder="Based on Document ID">
-			<? $nodes = $document->getNodes(); ?>
-			<? if($nodes): ?>
-				<? foreach($nodes as $key => $node): ?>
-					<? $node->display_edit($key); ?>
-				<? endforeach; ?>
-			<? endif; ?>
+			<div id="sortable">
+				<? $nodes = $document->getNodes(); ?>
+				<? if($nodes): ?>
+					<? foreach($nodes as $key => $node): ?>
+						<? $node->display_edit($key); ?>
+					<? endforeach; ?>
+				<? endif; ?>
+			</div>
 			<button type="submit" name="submit" value="edit">Submit</button>
 			<button type="delete" name="submit" value="delete">Delete Document</button>
 		</form>
+		<script type="text/javascript">
+		  $(function() {
+		    $( "#sortable" ).sortable({
+					stop: function(event, ui) {
+						$("#sortable .module").each(function(i) {
+							var index = $(this).index();
+							$(this).find("[name]").each(function(i, el) {
+								var text = $(this).attr("name");
+								var attrName = text.substring(text.indexOf("]")+1,text.length);
+								var newName = "nodes["+index+"]"+attrName;
+								$(this).attr("name",newName);
+							});
+						});
+					}
+				});
+		    $( "#sortable" ).disableSelection();
+		  });
+		</script>
 	</div>
 	<div class="preview">
 		<? $nodes = $document->getNodes(); ?>
