a2b985a80be0e4c41a0e12261fb777c2443497a5
Stefterv <stefterv@stefterv.local>
2015-06-01 17:24:57 +0200

    sync!

--- static/documents.php
+++ static/documents.php
@@ -1,8 +1,26 @@
 <? include "header.php" ?>
 <main>
 	<div class="container">
-		<div class="navigation">
-			navigation
+		<div class="navigation document_overview">
+			<div class="search" contenteditable>seachbar</div>
+			<div class="sort">sort by <a href="#">name</a>/<a href="#">date added</a>/<a href="#">date modified</a></div>
+			<div class="document_container">
+				<div class="document">
+					Document #1
+				</div>
+				<div class="document">
+					Document #2
+				</div>
+				<div class="document indent-1">
+					Document #3 indent
+				</div>
+				<? for($i = 4; $i<50; $i++): ?>
+				<div class="document">
+					Document #<?=$i?>
+				</div>
+				<? endfor;?>
+			</div>
+			<a href="document_edit">new document</a>
 		</div>
 		<div class="preview">
 			preview
