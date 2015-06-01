72e7a55a5f61ea74697aeabccba4c394a3865ac1
Willem Kempers <willemkempers@gmail.com>
2015-06-01 18:39:27 +0200

    document_modules

--- static/singledocument.php
+++ static/singledocument.php
@@ -1,21 +1,37 @@
+<? $page = "documents" ?>
 <? include "header.php" ?>
 <main>
 	<div class="container">
 		<div class="navigation document_overview">
-			<div class="search" contenteditable>Title</div>
-			<div class="module_container">
-				<h2>Modules</h2>
+			<div class="title" contenteditable>Title</div>
+			<div class="document_module_container">
+				<h2>MODULES</h2>
+				<div class="insertModule">
+					<div class="insertMod">
+						<div class="addButton">+</div>
+						<p>insert module</p>
+					</div>
+					<div class="insertText">
+						<div class="addButton">+</div>
+						<p>insert text/image</p>
+					</div>
+				</div>
 				<? for($i = 0; $i<10; $i++): ?>
 				<div class="module">
 					<p>Module #<?=$i?></p>
-					<div class="button edit">edit</div>
-					<div class="button remove">x</div>
+					<div class="modulebutton edit">edit</div>
+					<div class="modulebutton remove">x</div>
 				</div>
 				<? endfor;?>
-			</div>	
+			</div>
+			<div class="module_bottom_buttons">
+				<div class="button modulebutt">View Booklet</div>
+				<div class="button modulebutt">Send for Verification</div>
+			</div>
 		</div>
 		<div class="preview">
-			preview
+			<div class="document_preview">&nbsp</div>
+			<div class="button deleteButton">Delete</div>
 		</div>
 	</div>
 </main>
