a32d9d8de64cb09f70f8d0b2a1724c4d92cf5751
cspoels <mail@casspoelstra.nl>
2015-06-01 22:31:36 +0200

    module library dynamisch gemacht jawol
    Nee echt, van statisch naar dynamisch inclusief ajax en jquery.

--- src2/pages/modules.php
+++ src2/pages/modules.php
@@ -1,126 +1,48 @@
-<?
-$modules = Module::findByAttr(array("master="=>"1"));
-$tags = Tag::findAll();
+<? $page = "modules" ?>
+<? $modules = Module::findByAttr(array("master="=>"1")); ?>
 
-
-?>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 <script>
 	$( document ).ready(function() {
-	    $( '#module_list' ).on( 'click', '.row', function () { // on click module
+	    $( '.document_container' ).on( 'click', '.module_item', function () { // on click module
 	    	var id = this.getAttribute('data-id');	// get id of clicked module
 	    	$.ajax({
 		        url : 'api/get_modules.php', // url of php file
 		        type : 'POST', // post
 		        success : function (result) { // when succesful
 		        	var j = JSON.parse(result); //parse from json
-		        	$('#preview_content').html((j[id])); // replace #preview_content with content from json
+		        	$('.module_preview').html((j[id])); // replace #preview_content with content from json
 		        },
 		        error : function () {
-		           $('#preview_content').html('Unable to load'); // if unable to load display this error message
+		           $('.document_container').html('Unable to load'); // if unable to load display this error message
 		        }
 		    })
 	    	
 	    });
 	});
 </script>
-<style type="text/css" media="screen">
-	
-	header{
-		width: 50%;
-	}
-	#interface{
-		position: absolute;
-		height: calc(100% - 100px);
-		overflow: scroll;
-		left: 0;
-		right: 50%;
-		display: inline-block;
-		border:1px solid black;
 
-	}
-	#tagging{
-		
-		border:1px solid black;
-		width:40%;
-		display: inline-block;
-	}
-	#module_list{
-		
-		border:1px solid black;
-		width:auto;
-		display: inline-block;
-	}
-
-	.row{
-		margin: 5px;
-		padding: 5px;
-		border:1px solid black;
-	}
-	.module_title{
-		text-decoration: underline;
-	}
-
-	.module_content{
-		padding: 10px;
-		background-color: #D7D7D7;
-	}
-
-	#preview{
-		position: absolute;
-		left: 50%;
-
-		height: 100%;
-		display: inline-block;
-	}
-	#preview_background{
-		background-color: #D7D7D7;
-		padding: 20px;
-	}
-
-	#preview_content{
-		background-color: white;
-		padding: 20px;
-	}
-	footer{
-		position: absolute;
-		background-color: white;
-		bottom: 0;
-		padding: 5px;
-	}
-</style>
-
-<main class="homepage">
-	<div id="interface">
-			
-	
-		<div id="tagging">
-			<? foreach($tags as $tag): ?>
-				<div class="row" data-indent="0">
-				<p><?=$tag->title;?></p>
+<main>
+	<div class="container">
+		<div class="navigation module_overview">
+			<div class="search" contenteditable>seachbar</div>
+			<div class="sort">sort by <a href="#">name</a>/<a href="#">date added</a>/<a href="#">date modified</a></div>
+			<div class="document_container">
+				<? foreach($modules as $module): ?>
+				<div class="module_item" data-id=<?=$module->id;?>>
+					<?=$module->title;?>
+				<? $date = date_create_from_format("Y-m-d H:i:s",$module->changed); ?>
+				
+				<div class="last_edit"><?=date_format($date, "H:i - d M");?></div>
 				</div>
 			<? endforeach; ?>
+				
+			</div>
 		</div>
-		<div id="module_list">
-			<? foreach($modules as $module): ?>
-
-				<div class="row" data-indent="0" data-id="<?=$module->id?>">
-					<p class="module_title"><?=$module->title;?></p>
-
-					<p class="module_changed"><?=$module->changed;?></p>
-					
-					<a href="module_edit?id=<?=$module->id?>" class="edit">Edit</a>
-				</div>
-			<? endforeach; ?>
+		<div class="preview">
+			<div class="module_preview">
+				<div class="default_preview_text">Click on a module on the left hand side to select it for editing.</div>
+			</div>
 		</div>
 	</div>
-
-	<div id="preview">
-		<div id="preview_background">
-			<div id="preview_content"></div>
-		</div>
-	</div>
-	<footer>
-		<a href="/module_edit">Add</a>
-	</footer>
 </main>
