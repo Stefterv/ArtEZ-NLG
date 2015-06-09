ed6159a0cf4ef8cc9f08d7b94fcc9287f3326c73
Willem Kempers <willemkempers@gmail.com>
2015-06-09 13:18:29 +0200

    pdf

---
+++ playground/pdf/document.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>NLG Document</title>
+		<link rel="stylesheet" href="style.css">
+	</head>
+
+	<body>
+		<main>
+			<header>
+			</header>
+			
+			<article>
+				<h1>Module 1</h1>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse nulla quam, porttitor eget imperdiet ac, eleifend et nibh. Fusce in dolor posuere urna sollicitudin fringilla vel non felis. Aliquam consequat convallis dolor, eu condimentum odio imperdiet rutrum. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Morbi aliquam turpis ac nulla tincidunt, eu scelerisque leo condimentum. Proin sed augue turpis. Quisque at pulvinar metus. Duis metus nibh, scelerisque quis facilisis ultricies, luctus non justo. Curabitur eget convallis neque, consectetur vehicula orci. Nunc faucibus, eros nec consequat porttitor, enim nibh auctor enim, id convallis enim purus at lacus. Proin facilisis vestibulum nisi cursus rutrum. Vestibulum vehicula fringilla ligula, ac pulvinar nunc aliquam at. Curabitur ac justo volutpat, lobortis ex eget, vulputate nulla. Interdum et malesuada fames ac ante ipsum primis in faucibus.</p>
+			</article>
+
+			<article>
+				<h1>Module 2</h1>
+				<p>Ut fringilla sapien molestie, ullamcorper massa ac, porttitor turpis. Fusce enim ipsum, fermentum vel molestie at, lacinia sit amet enim. Donec odio dui, facilisis quis ullamcorper id, aliquet quis nisl. Aliquam condimentum risus eget ante semper, eu fringilla lorem molestie. Suspendisse vestibulum, justo vitae eleifend pellentesque, nisl metus rhoncus neque, et iaculis orci leo ut nulla. Nunc et ex faucibus, volutpat leo at, vulputate dolor. Integer molestie, lectus et tempus gravida, ligula leo euismod tellus, ut egestas est est in erat. Maecenas eget tincidunt mauris. Nam eu sapien rhoncus, lacinia orci vel, tempus leo. Pellentesque felis diam, rhoncus ac scelerisque id, eleifend sit amet ante. Nullam porta sollicitudin dignissim. Phasellus a erat accumsan, aliquet risus ac, fringilla tellus. Aliquam erat volutpat. Cras dictum purus vel odio finibus accumsan. Maecenas consectetur dolor ac ante cursus facilisis. Maecenas quis posuere augue, a venenatis neque.</p>
+			</article>
+
+			<article>
+				<h1>Module 3</h1>
+				<p>In hac habitasse platea dictumst. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nam a elit sollicitudin, tempus lectus vitae, venenatis quam. Sed tristique tellus sed nisl tristique, ac auctor enim sodales. Pellentesque malesuada, tellus blandit pellentesque lacinia, erat elit vehicula nisi, ac rhoncus magna erat nec sem. Vestibulum efficitur nisl eu viverra egestas. Donec et ultricies lorem. In posuere tempus ex vel sodales. Nullam in risus nec urna egestas tempor in et mauris. Vivamus elementum ex non lorem eleifend pellentesque. Nunc ut risus ac odio dictum sagittis in in eros. Nam faucibus euismod mauris vel congue.</p>
+			</article>
+
+			<article>
+				<h1>Module 4</h1>
+				<p>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam id molestie nunc, et tempus risus. Aenean egestas, nunc eu finibus rhoncus, nisi libero auctor massa, vitae fermentum metus elit in tellus. In vel velit quis turpis aliquet sodales id id lorem. Duis ut dictum est, eu commodo nunc. Morbi aliquet ultricies mi et lacinia. Integer scelerisque justo eu nunc lobortis sagittis.</p>
+			</article>
+
+			<article>
+				<h1>Module 5</h1>
+				<p>Nulla vehicula eros ut urna rhoncus, nec finibus neque porttitor. Nam rutrum vestibulum lacus, tincidunt fermentum elit rutrum fringilla. Integer malesuada odio et turpis aliquet varius. Sed laoreet, augue in aliquet feugiat, elit massa euismod erat, a fermentum libero libero vitae ligula. Ut vulputate, purus sed semper sodales, metus quam luctus urna, vel fringilla ipsum nisi convallis est. Aenean maximus est neque, non congue justo commodo non. Quisque a semper felis.</p>
+			</article>
+
+			<footer>
+			</footer>
+		</main>
+	</body>
+
+</html>
