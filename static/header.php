6b4ff36145df7eef416536e9b15dde854df7d81e
Missy <missyskae@gmail.com>
2015-06-11 14:40:07 +0200

    header veranderingen

--- static/header.php
+++ static/header.php
@@ -7,6 +7,8 @@
 </head>
 <body>
 <header>
-	<a <?if($page=="documents"):?>class="current"<?endif;?> href="documents.php">Documents</a>
-	<a <?if($page=="modules"):?>class="current"<?endif;?>href="modules.php">Modules</a>
+		<a id="modulesbutton"<?if($page=="modules"):?>class="current"<?endif;?>href="modules.php">MODULES</a>
+		<a id="documentbutton"<?if($page=="documents"):?>class="current"<?endif;?> href="documents.php">DOCUMENTS</a>
+
+		<a id="createnew"><sup>+ </sup>create new <?if($page=="modules"):?>module<?endif;?><?if($page=="documents"):?>document<?endif;?></a>
 </header>
