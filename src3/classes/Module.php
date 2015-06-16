2f5276b1394f845b2f25be8ee9ed0bb287d06858
cspoels <mail@casspoelstra.nl>
2015-06-16 14:15:00 +0200

    icoontjes op document edit en stroke verwijderd

--- src3/classes/Module.php
+++ src3/classes/Module.php
@@ -21,16 +21,17 @@ class Module extends databaseObject{
 				
 				<div class="module_text" contenteditable data-inputvalue="content" data-placeholder="Text"><?=$this->content?></div>
 				<div class="module_buttons">
-					<a href="#" class="module_move">reorder</a>
-					<a href="#" class="module_edit">edit</a>
-					<a href="#" class="module_remove">x</a>
+					<!-- <a href="#" class="module_move">reorder</a> -->
+					<a href="#" alt="Re-order module" class="module_move"><?=file_get_contents("media/icons/rearrange3.svg")?></a>
+					<a href="#" alt="Edit module" class="module_edit"><?=file_get_contents("media/icons/edit_icon.svg")?></a>
+					<a href="#" alt="Delete module" class="module_remove"><?=file_get_contents("media/icons/binoption2.svg")?></a>
 				</div>
 		</div>
 		<?
 	}
 	function display_preview(){
 		?>
-		
+
 		<div class="preview_subtitle"><?=$this->title?></div>
 		<p><?=$this->content?></p>
 		<?
