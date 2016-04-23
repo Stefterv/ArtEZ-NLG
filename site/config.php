3bfca464ceae6f76893d5c4381f5024af99c4765
Daniel Powers <dancepowers@gmail.com>
2016-04-23 15:27:58 +0200

    Added DB

--- site/config.php
+++ site/config.php
@@ -4,7 +4,7 @@ date_default_timezone_set('America/New_York');
 
 define("DB_LOC", "localhost");
 define("DB_USER", "root");
-define("DB_PASS", "nintendo");
+define("DB_PASS", "root");
 define("DB_NAME", "nlg");
 
 define("DISPLAY_DATE_FORMAT", "Y-m-d");
