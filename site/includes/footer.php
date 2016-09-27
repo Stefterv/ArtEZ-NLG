e51b45ff6155fca5160cc5ddd1c9186842fa06d1
Stefterv <stefterv@stefterv.local>
2016-09-27 16:00:59 +0200

    feedback system 0.0.1

--- site/includes/footer.php
+++ site/includes/footer.php
@@ -1,4 +1,11 @@
 <div class="footergradient"></div>
 
+<?
+$info = "DEBUG INFO:\n";
+$info .= $_SERVER['HTTP_USER_AGENT']."\n";
+$info .= $_SERVER['REQUEST_URI']."\n";
+$info = rawurlencode($info);
+?>
+<div class="feedback"><a href="mailto:stef@steftervelde.nl?subject=NLG%20Toolkit%20Issue&body=<?=$info?>">beta</a></div>
 </body>
 </html>
